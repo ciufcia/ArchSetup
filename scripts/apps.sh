db83cf5bfee5e8bddd26954e7f0ef348ba0e141a
ciufcia <ja.satola@gmail.com>
2025-10-28 19:17:30 +0100

    .

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -10,3 +10,11 @@ cd ..
 rm -rf yay
 
 yay -S --noconfirm --needed visual-studio-code-bin librewolf-bin foot
+
+mkdir -p ~/.config/foot
+ln -sf ~/ArchSetup/configs/foot/foot.ini ~/.config/foot/foot.ini
+
+sed -i '/^[[:space:]]*\(export[[:space:]]\+\)\?PS1=/d' "$HOME/.bashrc" || true
+cat >> "$HOME/.bashrc" <<'BASHRC_PS1'
+PS1='\[\e[1;32m\]❮\u@\h❯\[\e[0m\] \[\e[1;34m\]❮\w❯\[\e[0m\] \[\e[1;35m\]➜\[\e[0m\] '
+BASHRC_PS1
