97bc05cd8b458231582f595aac8fc95ce890217a
ciufcia <ja.satola@gmail.com>
2025-10-29 11:40:32 +0100

    .

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -4,13 +4,15 @@ set -eo pipefail
 
 echo "Installing Hyprland and configuring..."
 
-sudo pacman -S --noconfirm --needed hyprland hyprpolkitagent hyprpaper
+sudo pacman -S --noconfirm --needed hyprland hyprpolkitagent hyprpaper waybar
 
 hyprpm update
-hyprpm add https://github.com/hyprwm/hyprland-plugins
-
-hyprpm enable hyprbars
+hyprpm add https://github.com/hyprwm/hyprland-plugins || true
 
 mkdir -p ~/.config/hypr
 ln -sf ~/ArchSetup/configs/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sf ~/ArchSetup/configs/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+
+mkdir -p ~/.config/waybar
+ln -sf ~/ArchSetup/configs/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -sf ~/ArchSetup/configs/waybar/style.css ~/.config/waybar/style.css
